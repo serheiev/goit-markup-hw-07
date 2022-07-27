6b35aed3542666761ace2568250b35a2ee151ab5
serheiev <69833095+serheiev@users.noreply.github.com>
2022-07-27 10:10:22 +0300

    adaptive it

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,20 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    const: body = document.querySelector('body')
   };
+
+
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    body.classList.toggle('no-scroll');
   }
+
+
 })();
+
+
